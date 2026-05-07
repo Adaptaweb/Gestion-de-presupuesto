6442305c6fe4f0d0850745d0122a0bafb171fd58
Adaptaweb <a.tamayo.web@gmail.com>
2026-05-07 08:44:34 -0400

    IA funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -122,7 +122,7 @@ import Login from './Login.jsx';
 import Register from './Register.jsx';
 import AdminPanel from './AdminPanel.jsx';
 
-const apiKey = "";
+const apiKey = "AIzaSyAQZ6oxdx2URjAyVAfK6J7vVBstTUbcE1A";
 
 const MONTH_NAMES = [
   'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
@@ -836,7 +836,7 @@ const Dashboard = ({ user, token, onLogout, onOpenAdmin }) => {
 
     while (retries < maxRetries) {
       try {
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${apiKey}`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
